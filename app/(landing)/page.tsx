55c405de8f085abcdfa25a63b465633affa0de55
imrulkayessifat <kayesimrul224@gmail.com>
2023-10-14 20:25:15 +0600

    trying to fix hosting error

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,15 +6,16 @@ import { redirect, useRouter } from 'next/navigation'
 import { getServerSession } from "next-auth/next"
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import DashboardConnector from '@/components/dashboard-connector'
-
+import getCurrentUser from '../actions/getCurrentUser'
 async function getSession() {
     return await getServerSession(authOptions)
 }
 
 
 const LandingPage = async () => { 
-  const session = await getSession();
-  if(session) redirect("dashboard")
+  const currentUser = await getCurrentUser();
+  console.log(currentUser)
+  if(currentUser?.id) redirect("dashboard")
   
   return (
     <div className='flex items-center justify-center h-full'>
