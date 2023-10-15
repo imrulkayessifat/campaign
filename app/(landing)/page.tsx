b33981d61fe675a87b0a610f1e5aa02e8cf9a1d0
imrulkayessifat <kayesimrul224@gmail.com>
2023-10-15 15:40:07 +0600

    fix...

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import { redirect, useRouter } from 'next/navigation'
-
 import { getServerSession } from "next-auth/next"
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import DashboardConnector from '@/components/dashboard-connector'
